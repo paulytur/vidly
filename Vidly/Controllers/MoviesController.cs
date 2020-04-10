bdefaa3939e2f2c8b4fe74e1daec1c1a08ff25ef
Paul MATTHEW <paul.matthew@edenred.com>
2020-04-10 23:31:18 +0800

    Remove comment

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,7 +10,6 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        ////Movies 
         public ActionResult Index()
         {
             var movies = GetMovies();
@@ -41,35 +40,15 @@ namespace Vidly.Controllers
                 Movie = movie,
                 Customers = customers
             };
-            //ViewData["Movie"] = movie;
-            //ViewBag.Movie = movie;
 
-            //var viewResult = new ViewResult();
-            //viewResult.ViewData.Model
-                
             return View(viewModel);
 
-            //return Content("Hello World"); 
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
 
         }
 
         public ActionResult Edit(int id) {
             return Content("id=" + id);
         }
-
-        //Movies 
-        //public ActionResult Index(int? pageIndex, string sortBy)
-        //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (String.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-
-        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month) {
